d1948fd2876ddb2c3fbc5963b8b3a9adfb8e372a
MarkulakS <markulaks17@gmail.com>
2024-01-25 14:22:14 +0100

    created responsive website

--- App/src/app/components/header/header.component.ts
+++ App/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PersonalDataService } from 'src/app/services/personal-data.service';
 
 @Component({
